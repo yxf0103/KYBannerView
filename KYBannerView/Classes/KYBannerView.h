459d78f145db4e534d7b6cac70d6707cd4ae992a
yxf <yanxuefeng@yxfdeMacBook-Pro.local>
2019-04-12 14:40:22 +0800

    a banner view

---
+++ KYBannerView/Classes/KYBannerView.h
@@ -0,0 +1,16 @@
+//
+//  KYBannerView.h
+//  KYBannerView
+//
+//  Created by yxf on 2019/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KYBannerView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KYBannerView/Classes/KYBannerView.m
@@ -0,0 +1,20 @@
+//
+//  KYBannerView.m
+//  KYBannerView
+//
+//  Created by yxf on 2019/4/12.
+//
+
+#import "KYBannerView.h"
+
+@implementation KYBannerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
